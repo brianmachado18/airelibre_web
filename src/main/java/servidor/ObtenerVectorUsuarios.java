c9d236fc43f7f808d80a9b559284df708eddc54c
brianmachado18 <brian.machado2180@gmail.com>
2024-11-01 14:04:28 -0300

    Ahora si

---
+++ src/main/java/servidor/ObtenerVectorUsuarios.java
@@ -0,0 +1,32 @@
+
+package servidor;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for obtenerVectorUsuarios complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="obtenerVectorUsuarios"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "obtenerVectorUsuarios")
+public class ObtenerVectorUsuarios {
+
+
+}
